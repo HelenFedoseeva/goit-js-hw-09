0b5e533a8fbf633a2e2aaec412456e4d1782e294
Helen <helenlena52@gmail.com>
2022-10-03 16:54:01 +0300

    add-first-resolve

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,16 @@
+const startBtnRef = document.querySelector('button[data-start]')
+const stopBtnRef = document.querySelector('button[data-stop]')
+const bodyRef = document.querySelector('body')
 
+
+
+startBtnRef.addEventListener('click', onStartClickHandler)
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onStartClickHandler() {
+    bodyRef.style.backgroundColor = getRandomHexColor()
+}
