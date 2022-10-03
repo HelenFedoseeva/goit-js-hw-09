d58c7b0d2daeb7f21b7e94b1c6f7315812f9b978
Helen <helenlena52@gmail.com>
2022-10-03 17:32:21 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,16 +1,29 @@
-const startBtnRef = document.querySelector('button[data-start]')
-const stopBtnRef = document.querySelector('button[data-stop]')
-const bodyRef = document.querySelector('body')
+const startBtnRef = document.querySelector('button[data-start]');
+const stopBtnRef = document.querySelector('button[data-stop]');
+const bodyRef = document.querySelector('body');
 
+startBtnRef.addEventListener('click', onStartClickHandler);
+stopBtnRef.addEventListener('click', onStopClickHandler);
 
-
-startBtnRef.addEventListener('click', onStartClickHandler)
-
+let isIntIsSet;
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
 function onStartClickHandler() {
-    bodyRef.style.backgroundColor = getRandomHexColor()
+  startBtnRef.setAttribute('disabled', true);
+  isIntIsSet = setInterval(changeColorInt, 1000);
+}
+
+function changeColorInt() {
+  bodyRef.style.backgroundColor = getRandomHexColor();
+}
+
+function onStopClickHandler() {
+  if (!isIntIsSet) {
+    return;
+  }
+  clearInterval(isIntIsSet);
+  startBtnRef.removeAttribute('disabled', true);
 }
