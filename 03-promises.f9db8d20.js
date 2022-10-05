e28998b4dccdb3477818f1db8861c38222d733d3
HelenFedoseeva <101935002+HelenFedoseeva@users.noreply.github.com>
2022-10-05 13:22:07 +0000

    Deploying to gh-pages from @ HelenFedoseeva/goit-js-hw-09@25d0922fa1e824c7bd515fe161a209a00d1225cd 🚀

---
+++ 03-promises.f9db8d20.js
@@ -0,0 +1,2 @@
+const e={firstDelayRef:document.querySelector('.form input[name="delay"]'),delayStepref:document.querySelector('.form input[name="step"]'),amountRef:document.querySelector('.form input[name="amount"]'),btnSubmitRef:document.querySelector("button")};console.log(e.btnSubmitRef),e.btnSubmitRef.addEventListener("submit",(function(e,t){Math.random()}));
+//# sourceMappingURL=03-promises.f9db8d20.js.map
