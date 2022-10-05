25d0922fa1e824c7bd515fe161a209a00d1225cd
Helen <helenlena52@gmail.com>
2022-10-05 16:21:43 +0300

    add-form-refs

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,5 +1,15 @@
-import { Confirm } from 'notiflix/build/notiflix-confirm-aio';
+// import { Confirm } from 'notiflix/build/notiflix-confirm-aio';
 
+const refs = {
+  firstDelayRef: document.querySelector('.form input[name="delay"]'),
+  delayStepref: document.querySelector('.form input[name="step"]'),
+  amountRef: document.querySelector('.form input[name="amount"]'),
+  btnSubmitRef: document.querySelector('button'),
+};
+
+console.log(refs.btnSubmitRef);
+
+refs.btnSubmitRef.addEventListener('submit', createPromise);
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
@@ -8,3 +18,5 @@ function createPromise(position, delay) {
     // Reject
   }
 }
+
+function onSubmitHandler() {}
