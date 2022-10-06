eeccb67d70797100bad939081d98dc02254c1550
HelenFedoseeva <101935002+HelenFedoseeva@users.noreply.github.com>
2022-10-06 14:04:45 +0000

    Deploying to gh-pages from @ HelenFedoseeva/goit-js-hw-09@ed0646adb1f111d66d78269a931fb2ba451593db 🚀

--- 03-promises.5bc20538.js
+++
@@ -1,2 +0,0 @@
-!function(){var e={firstDelayRef:document.querySelector('.form input[name="delay"]'),delayStepref:document.querySelector('.form input[name="step"]'),amountRef:document.querySelector('.form input[name="amount"]'),btnSubmitRef:document.querySelector("button")},t=null;e.btnSubmitRef.addEventListener("submit",(function(){t=e.amountRef.value;e.delayStepref.value,setInterval((position,void(Math.random()>.3?console.log("Это лог удачного промиса"):console.log("это лог неудачного промиса"))),t)}))}();
-//# sourceMappingURL=03-promises.5bc20538.js.map
