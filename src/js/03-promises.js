7e823079f2d4f9a1635e946d31063967bec623af
Helen <helenlena52@gmail.com>
2022-10-09 00:23:38 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,26 +1,46 @@
-// import { Confirm } from 'notiflix/build/notiflix-confirm-aio';
+import Notiflix from 'notiflix';
 
-const refs = {
-  formRef: document.querySelector('.form'),
-  firstDelayRef: document.querySelector('.form input[name="delay"]'),
-  delayStepref: document.querySelector('.form input[name="step"]'),
-  amountRef: document.querySelector('.form input[name="amount"]'),
-  btnSubmitRef: document.querySelector('button'),
-};
+const formRef = document.querySelector('.form');
+// console.log(formRef);
 
-refs.formRef.addEventListener('submit', onSubmitHandler);
+formRef.addEventListener('submit', onSubmitHandler);
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    const objectPromise = { position, delay };
+
+    if (shouldResolve) {
+      resolve(objectPromise);
+    } else {
+      reject(objectPromise);
+    }
+  });
+}
 
 function onSubmitHandler(e) {
   e.preventDefault();
-  let counter = 0;
-  counter += 1;
-  let amount = refs.amountRef.value;
+  console.log(123);
+  let delay = e.target.delay.value;
+  let amount = e.target.amount.value;
+  let step = e.target.step.value;
 
-  if (counter > amount) {
-    clearInterval(promiseID);
+  for (let position = 1; position <= amount; position += 1) {
+    createPromise(2, 1500)
+      .then(({ position, delay }) => {
+        setTimeout(() => {
+          Notiflix.Notify.success(
+            `✅ Fulfilled promise ${position} in ${delay}ms`
+          );
+        }, delay);
+      })
+      .catch(({ position, delay }) => {
+        setTimeout(() => {
+          Notiflix.Notify.failure(
+            `❌ Rejected promise ${position} in ${delay}ms`
+          );
+        }, delay);
+      });
+    delay += step;
   }
-
-  const promiseID = setInterval(createPromise, amount);
 }
-
-function createPromise() {}
