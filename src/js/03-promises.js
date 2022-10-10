85f79dcd51e5d354c51c77f1decadeaedd514b7f
Helen <helenlena52@gmail.com>
2022-10-10 16:16:21 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,7 +1,9 @@
 import Notiflix from 'notiflix';
 
 const formRef = document.querySelector('.form');
-// console.log(formRef);
+const amountRef = document.querySelector('input[name="amount"]');
+const delayRef = document.querySelector('input[name="delay"]');
+const stepRef = document.querySelector('input[name="step"]');
 
 formRef.addEventListener('submit', onSubmitHandler);
 
@@ -15,18 +17,19 @@ function createPromise(position, delay) {
     } else {
       reject(objectPromise);
     }
+
+    // delay += step;
   });
 }
 
 function onSubmitHandler(e) {
   e.preventDefault();
-  console.log(123);
-  let delay = e.target.delay.value;
-  let amount = e.target.amount.value;
-  let step = e.target.step.value;
+  let delay = delayRef.value;
+  let amount = amountRef.value;
+  let step = stepRef.value;
 
   for (let position = 1; position <= amount; position += 1) {
-    createPromise(2, 1500)
+    createPromise(position, delay)
       .then(({ position, delay }) => {
         setTimeout(() => {
           Notiflix.Notify.success(
@@ -41,6 +44,7 @@ function onSubmitHandler(e) {
           );
         }, delay);
       });
-    delay += step;
+    // Number((delay += step));
   }
+  // delay += step;
 }
